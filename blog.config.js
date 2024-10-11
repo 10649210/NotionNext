0f32213f4e7f9cfccd99f57d2b21bf3bb290a748
云波萦涣 <99639779+10649210@users.noreply.github.com>
2024-10-12 00:58:29 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -22,7 +22,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '瀛洲小虎', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '瀛洲山木', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'BMS攻城狮🍚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.techyyyh.eu.org/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '电池, BMS, 博客', // 网站关键词 英文逗号隔开
